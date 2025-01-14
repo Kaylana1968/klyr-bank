b06e380af41163be45fed5fd1b91f7c1a72d205d
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2025-01-14 10:49:55 +0100

    login working

--- src/auth/controller/register.py
+++ src/auth/controller/register.py
@@ -4,6 +4,7 @@ import jwt
 import os
 from dotenv import load_dotenv
 from sqlmodel import select
+from passlib.context import CryptContext
 
 from database.init import get_session
 from database.models import User
@@ -17,28 +18,43 @@ secret_key = os.getenv("SECRET_KEY")
 bearer_scheme = HTTPBearer()
 algorithm = "HS256"
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def get_user(authorization: HTTPAuthorizationCredentials = Depends(bearer_scheme)):
     return jwt.decode(authorization.credentials, secret_key, algorithms=[algorithm])
 
 
 def generate_token(user: User):
-    return jwt.encode(user.dict(), secret_key, algorithm=algorithm)
+    return jwt.encode({"id": str(user.id)}, secret_key, algorithm=algorithm)
+
+
+def verify_password(password, hashed_password):
+    return pwd_context.verify(password, hashed_password)
+
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
 
 
 @router.post("/login")
-def login(user: User):
+def login(body: CreateUser, session=Depends(get_session)):
+    user = session.exec(select(User).where(User.email == body.email)).first()
+
+    if user == None:
+        return {"message": "User not found"}
+    elif not verify_password(password=body.password, hashed_password=user.password):
+        return {"message": "Password mismatch"}
+
     return {"token": generate_token(user)}
 
 
 @router.post("/register")
 def register(body: CreateUser, session=Depends(get_session)) -> User:
-    user = User(email=body.email, password=body.password)
+    user = User(email=body.email, password=hash_password(body.password))
     session.add(user)
     session.commit()
     session.refresh(user)
-
-    return user
 
     return {"token": generate_token(user)}
 
