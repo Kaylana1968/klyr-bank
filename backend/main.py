7d3fb2f8c15eaee2f425504f7bfaa10701197c40
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2025-01-21 14:49:45 +0100

    linked back with front

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from database.init import create_db_and_tables
 from src.router import define_routes
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
 
 
-@app.get("/api/")
+@app.get("/api")
 def read_root():
     raise HTTPException(status_code=200, detail="Dans le beurre y a de l'huile")
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import "./index.css";
 
 export default function App() {
+	useEffect(() => {
+		fetch("http://127.0.0.1:8000/api", {
+			method: "GET"
+		})
+			.then(res => res.json())
+			.then(res => console.log(res));
+	}, []);
+
 	return (
 		<div className="m-4">
 			<h1>Hello, Vite and React!</h1>
